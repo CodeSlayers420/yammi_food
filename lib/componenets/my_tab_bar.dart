9741ddc6b275da4c4591f0443007c1acde10cfb2
Md Noman Ahmed Joy <nomanjoy9925@gmail.com>
2025-10-29 00:00:32 +0600

    models

---
+++ lib/componenets/my_tab_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/models/food.dart';
+
+class MyTabBar extends StatelessWidget {
+  final TabController tabController;
+
+  const MyTabBar({super.key, required this.tabController});
+
+  List<Tab> _buildCategoryTabs() {
+    return FoodCategory.values.map((category) {
+      return Tab(icon: Icon(category.icon, size: 20), text: category.label);
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      controller: tabController,
+      isScrollable: true,
+      tabs: _buildCategoryTabs(),
+      indicatorColor: Theme.of(context).colorScheme.primary,
+      labelColor: Theme.of(context).colorScheme.primary,
+      unselectedLabelColor: Theme.of(
+        context,
+      ).colorScheme.onSurface.withOpacity(0.5),
+    );
+  }
+}
